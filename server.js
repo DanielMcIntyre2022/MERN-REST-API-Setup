dcf061b53cf2d55a7131c59ed610f4a1916e21bb
Daniel <daniel-mcintyre@hotmail.com>
2022-11-29 13:43:59 -0500

    establish connection with MongoDB Atlas

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT;
+const db = require('./config/db');
+
+db();
 
 const app = express();
 
