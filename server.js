de6d7efea85068380d74c97ba2b52572e2b98208
Daniel <daniel-mcintyre@hotmail.com>
2022-11-29 13:33:13 -0500

    initial server setup

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT;
+
+const app = express();
+
+app.listen(port, console.log(`listening on port: ${port}`));
